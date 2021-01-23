2e5a05d88e56418e7a01d65a932d424c6b15914b
froze0451 <frozealex@gmail.com>
2021-01-24 00:31:01 +0300

    improved guests dropdown styles, button added, added scripts for menu droping and styles for border

---
+++ dist/form_elements.dae2e72b472395381e86.js
@@ -0,0 +1,18 @@
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is not neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (function() { // webpackBootstrap
+/*!**********************************************!*\
+  !*** ./pages/form-elements/form-elements.js ***!
+  \**********************************************/
+/*! namespace exports */
+/*! exports [not provided] [no usage info] */
+/*! runtime requirements:  */
+eval("throw new Error(\"Module build failed (from ../node_modules/babel-loader/lib/index.js):\\nSyntaxError: D:\\\\Task-2\\\\src\\\\pages\\\\form-elements\\\\form-elements.js: Unexpected token, expected \\\",\\\" (33:3)\\n\\n\\u001b[0m \\u001b[90m 31 | \\u001b[39m\\u001b[0m\\n\\u001b[0m \\u001b[90m 32 | \\u001b[39m\\u001b[0m\\n\\u001b[0m\\u001b[31m\\u001b[1m>\\u001b[22m\\u001b[39m\\u001b[90m 33 | \\u001b[39m   \\u001b[0m\\n\\u001b[0m \\u001b[90m    | \\u001b[39m   \\u001b[31m\\u001b[1m^\\u001b[22m\\u001b[39m\\u001b[0m\\n    at Parser._raise (D:\\\\Task-2\\\\node_modules\\\\@babel\\\\parser\\\\lib\\\\index.js:748:17)\\n    at Parser.raiseWithData (D:\\\\Task-2\\\\node_modules\\\\@babel\\\\parser\\\\lib\\\\index.js:741:17)\\n    at Parser.raise (D:\\\\Task-2\\\\node_modules\\\\@babel\\\\parser\\\\lib\\\\index.js:735:17)\\n    at Parser.unexpected (D:\\\\Task-2\\\\node_modules\\\\@babel\\\\parser\\\\lib\\\\index.js:9097:16)\\n    at Parser.expect (D:\\\\Task-2\\\\node_modules\\\\@babel\\\\parser\\\\lib\\\\index.js:9083:28)\\n    at Parser.parseCallExpressionArguments (D:\\\\Task-2\\\\node_modules\\\\@babel\\\\parser\\\\lib\\\\index.js:10307:14)\\n    at Parser.parseCoverCallAndAsyncArrowHead (D:\\\\Task-2\\\\node_modules\\\\@babel\\\\parser\\\\lib\\\\index.js:10231:29)\\n    at Parser.parseSubscript (D:\\\\Task-2\\\\node_modules\\\\@babel\\\\parser\\\\lib\\\\index.js:10167:19)\\n    at Parser.parseSubscripts (D:\\\\Task-2\\\\node_modules\\\\@babel\\\\parser\\\\lib\\\\index.js:10139:19)\\n    at Parser.parseExprSubscripts (D:\\\\Task-2\\\\node_modules\\\\@babel\\\\parser\\\\lib\\\\index.js:10128:17)\");\n\n//# sourceURL=webpack:///./pages/form-elements/form-elements.js?");
+/******/ })()
+;
--- src/pages/form-elements/form-elements.js
+++ src/pages/form-elements/form-elements.js
@@ -2,8 +2,27 @@ import '../../styles/style.scss'
 import './form-elements.scss'
 import '../assets/logo.scss'
 
-const dropDowmMeny = document.querySelector('.dropdown-field')
+let dropDownField = document.querySelector('.dropdown-field')
+let dropDowmMenuButton = document.querySelector('.drop-button')
+let dropDownMenu = document.querySelector('.dropdown-menu')
 
-dropDowmMeny.addEventListener('click', () => {
-  console.log('nice one')
+dropDowmMenuButton.addEventListener('click', () => {
+  dropDownMenu.classList.toggle('invisible')
+
+  if (dropDownMenu.classList.contains('invisible')) {
+    dropDownField.style.borderRadius = '4px 4px 4px 4px';
+  } else {
+    dropDownField.style.borderRadius = '4px 4px 0 0';
+  }
 })
+
+dropDownField.addEventListener('mouseout', () => {
+  if (dropDownMenu.classList.contains('invisible')) {
+    dropDownField.style.border = '1px solid rgba(31, 32, 65, 0.25)';
+  } 
+})
+
+dropDownField.addEventListener('mouseover', () => {
+  dropDownField.style.border = '1px solid rgba(31, 32, 65, 0.5)';
+})
+
