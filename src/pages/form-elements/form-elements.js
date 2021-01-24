a1678633bbae62e95502563d23053807430bbccd
froze0451 <frozealex@gmail.com>
2021-01-25 00:13:30 +0300

    added masked text field. added imask plugin, added mask.

--- src/pages/form-elements/form-elements.js
+++ src/pages/form-elements/form-elements.js
@@ -1,7 +1,9 @@
 import '../../styles/style.scss'
 import './form-elements.scss'
 import '../assets/logo.scss'
+import IMask from 'imask';
 
+/* для dropdown с гостями */ 
 let dropDownField = document.querySelector('.dropdown-field')
 let dropDowmMenuButton = document.querySelector('.drop-button')
 let dropDownMenu = document.querySelector('.dropdown-menu')
@@ -25,4 +27,43 @@ dropDownField.addEventListener('mouseout', () => {
 dropDownField.addEventListener('mouseover', () => {
   dropDownField.style.border = '1px solid rgba(31, 32, 65, 0.5)';
 })
+
+
+/*для поля с маской
+let maskedField = document.querySelector('.text-field-masked input')*/
+
+let dateMask = IMask(
+  document.querySelector('.text-field-masked input'),
+  {
+    mask: Date,
+    min: new Date(1990, 0, 1),
+    max: new Date(2021, 0, 1),
+    lazy: true
+  });
+
+
+
+
+
+
+
+
+
+/*maskedField.addEventListener('input', () => {
+  if (maskedField.value == 911) {
+    console.log('OMG')
+  } else {
+    console.log(maskedField.value)
+  }
+})*/
+
+/*maskedField.addEventListener('input', () => {
+  if (maskedField.value.match(/^(?:(?:31(\.)(?:0?[13578]|1[02]))\1|(?:(?:29|30)(\.)(?:0?[1,3-9]|1[0-2])\2))(?:(?:1[6-9]|[2-9]\d)?\d{2})$|^(?:29(\.)0?2\3(?:(?:(?:1[6-9]|[2-9]\d)?(?:0[48]|[2468][048]|[13579][26])|(?:(?:16|[2468][048]|[3579][26])00))))$|^(?:0?[1-9]|1\d|2[0-8])(\.)(?:(?:0?[1-9])|(?:1[0-2]))\4(?:(?:1[6-9]|[2-9]\d)?\d{2})$/) !== null) {
+    console.log('yes')
+  } else {
+    console.log('nah')
+  }
+})*/
+
+    
 
