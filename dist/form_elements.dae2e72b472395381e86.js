4213db12c9e7d6cdee213595a33bd544d70f4e2a
froze0451 <frozealex@gmail.com>
2021-01-26 00:19:36 +0300

    finished scripts for dropdown guests

---
+++ dist/form_elements.dae2e72b472395381e86.js
@@ -0,0 +1,18 @@
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is not neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (function() { // webpackBootstrap
+/*!**********************************************!*\
+  !*** ./pages/form-elements/form-elements.js ***!
+  \**********************************************/
+/*! namespace exports */
+/*! exports [not provided] [no usage info] */
+/*! runtime requirements:  */
+eval("throw new Error(\"Module build failed (from ../node_modules/babel-loader/lib/index.js):\\nSyntaxError: D:\\\\Task-2\\\\src\\\\pages\\\\form-elements\\\\form-elements.js: Unexpected token (44:13)\\n\\n\\u001b[0m \\u001b[90m 42 | \\u001b[39m      \\u001b[36mreturn\\u001b[39m \\u001b[32m`${childs} младенец`\\u001b[39m\\u001b[0m\\n\\u001b[0m \\u001b[90m 43 | \\u001b[39m    } \\u001b[36melse\\u001b[39m \\u001b[36mif\\u001b[39m (childs \\u001b[33m>\\u001b[39m \\u001b[35m1\\u001b[39m \\u001b[33m&&\\u001b[39m childs \\u001b[33m<\\u001b[39m \\u001b[35m5\\u001b[39m \\u001b[33m||\\u001b[39m childs \\u001b[33m>\\u001b[39m \\u001b[35m21\\u001b[39m \\u001b[33m&&\\u001b[39m childs \\u001b[33m<\\u001b[39m \\u001b[35m25\\u001b[39m) {\\u001b[0m\\n\\u001b[0m\\u001b[31m\\u001b[1m>\\u001b[22m\\u001b[39m\\u001b[90m 44 | \\u001b[39m      \\u001b[36mreturn\\u001b[39m \\u001b[33m=\\u001b[39m \\u001b[32m`${childs} младенца`\\u001b[39m\\u001b[0m\\n\\u001b[0m \\u001b[90m    | \\u001b[39m             \\u001b[31m\\u001b[1m^\\u001b[22m\\u001b[39m\\u001b[0m\\n\\u001b[0m \\u001b[90m 45 | \\u001b[39m    } \\u001b[36melse\\u001b[39m {\\u001b[0m\\n\\u001b[0m \\u001b[90m 46 | \\u001b[39m      \\u001b[36mreturn\\u001b[39m \\u001b[33m=\\u001b[39m \\u001b[32m`${childs} младенцев`\\u001b[39m\\u001b[0m\\n\\u001b[0m \\u001b[90m 47 | \\u001b[39m    }\\u001b[0m\\n    at Parser._raise (D:\\\\Task-2\\\\node_modules\\\\@babel\\\\parser\\\\lib\\\\index.js:748:17)\\n    at Parser.raiseWithData (D:\\\\Task-2\\\\node_modules\\\\@babel\\\\parser\\\\lib\\\\index.js:741:17)\\n    at Parser.raise (D:\\\\Task-2\\\\node_modules\\\\@babel\\\\parser\\\\lib\\\\index.js:735:17)\\n    at Parser.unexpected (D:\\\\Task-2\\\\node_modules\\\\@babel\\\\parser\\\\lib\\\\index.js:9097:16)\\n    at Parser.parseExprAtom (D:\\\\Task-2\\\\node_modules\\\\@babel\\\\parser\\\\lib\\\\index.js:10548:20)\\n    at Parser.parseExprSubscripts (D:\\\\Task-2\\\\node_modules\\\\@babel\\\\parser\\\\lib\\\\index.js:10122:23)\\n    at Parser.parseUpdate (D:\\\\Task-2\\\\node_modules\\\\@babel\\\\parser\\\\lib\\\\index.js:10102:21)\\n    at Parser.parseMaybeUnary (D:\\\\Task-2\\\\node_modules\\\\@babel\\\\parser\\\\lib\\\\index.js:10091:17)\\n    at Parser.parseExprOps (D:\\\\Task-2\\\\node_modules\\\\@babel\\\\parser\\\\lib\\\\index.js:9961:23)\\n    at Parser.parseMaybeConditional (D:\\\\Task-2\\\\node_modules\\\\@babel\\\\parser\\\\lib\\\\index.js:9935:23)\");\n\n//# sourceURL=webpack:///./pages/form-elements/form-elements.js?");
+/******/ })()
+;
--- src/pages/form-elements/form-elements.js
+++ src/pages/form-elements/form-elements.js
@@ -3,10 +3,62 @@ import './form-elements.scss'
 import '../assets/logo.scss'
 import IMask from 'imask';
 
-/* для dropdown с гостями */ 
+/* переменные */ 
 let dropDownField = document.querySelector('.dropdown-field')
+let drdText = document.querySelector('.dropdown-field > p')
 let dropDowmMenuButton = document.querySelector('.drop-button')
 let dropDownMenu = document.querySelector('.dropdown-menu')
+
+let guests = 0;
+let childs = 0;
+
+/*функции*/ 
+function guestsCount(guests) {
+  if (guests < 21) {
+    if (guests === 0) {
+      return ''
+    } else if (guests === 1 || guests === 21) {
+      return `${guests} гость`
+    } else if (guests > 1 && guests < 5 || guests > 21 && guests < 25) {
+      return `${guests} гостя`
+    } else {
+      return `${guests} гостей`
+    }
+  } else {
+    if (/1$/.test(guests)) {
+      return `${guests} гость`
+    } else if (/[234]$/.test(guests)) {
+      return`${guests} гостя`
+    } else {
+      return `${guests} гостей`
+    }
+  }
+}
+
+function childsCount(childs) {
+  if (childs < 21) {
+    if (childs === 0) {
+      return ""
+    } else if (childs === 1 || childs === 21) {
+      return `, ${childs} младенец`
+    } else if (childs > 1 && childs < 5 || childs > 21 && childs < 25) {
+      return `, ${childs} младенца`
+    } else {
+      return `, ${childs} младенцев`
+    }
+  } else {
+    if (/1$/.test(childs)) {
+      return `, ${childs} младенец`
+    } else if (/[234]$/.test(childs)) {
+      return `, ${childs} младенца`
+    } else {
+      return `, ${childs} младенцев`
+    }
+  }
+}
+
+
+/*эвенты*/ 
 
 dropDowmMenuButton.addEventListener('click', () => {
   dropDownMenu.classList.toggle('invisible')
@@ -29,8 +81,7 @@ dropDownField.addEventListener('mouseover', () => {
 })
 
 
-/*для поля с маской
-let maskedField = document.querySelector('.text-field-masked input')*/
+/*маска для поля даты*/
 
 let dateMask = IMask(
   document.querySelector('.text-field-masked input'),
@@ -41,29 +92,117 @@ let dateMask = IMask(
     lazy: true
   });
 
+/*дропдаун меню кнопки добавить/убрать*/ 
 
+/*let removeButton = document.querySelector('.minus')
+let addButton = document.querySelector('.plus')
 
-
-
-
-
-
-
-/*maskedField.addEventListener('input', () => {
-  if (maskedField.value == 911) {
-    console.log('OMG')
-  } else {
-    console.log(maskedField.value)
+removeButton.addEventListener('click', () => {
+  if (Number(addButton.previousElementSibling.textContent) > 0) {
+    addButton.previousElementSibling.textContent = Number(addButton.previousElementSibling.textContent) - 1
   }
+})
+
+addButton.addEventListener('click', () => {
+  addButton.previousElementSibling.textContent = Number(addButton.previousElementSibling.textContent) + 1
 })*/
 
-/*maskedField.addEventListener('input', () => {
-  if (maskedField.value.match(/^(?:(?:31(\.)(?:0?[13578]|1[02]))\1|(?:(?:29|30)(\.)(?:0?[1,3-9]|1[0-2])\2))(?:(?:1[6-9]|[2-9]\d)?\d{2})$|^(?:29(\.)0?2\3(?:(?:(?:1[6-9]|[2-9]\d)?(?:0[48]|[2468][048]|[13579][26])|(?:(?:16|[2468][048]|[3579][26])00))))$|^(?:0?[1-9]|1\d|2[0-8])(\.)(?:(?:0?[1-9])|(?:1[0-2]))\4(?:(?:1[6-9]|[2-9]\d)?\d{2})$/) !== null) {
-    console.log('yes')
-  } else {
-    console.log('nah')
+
+/* подсчет младенцев */
+let removeChild = document.querySelector('.child-minus')
+let addChild = document.querySelector('.child-plus')
+
+removeChild.addEventListener('click', () => {
+  if (Number(removeChild.nextElementSibling.textContent) > 0) {
+    removeChild.nextElementSibling.textContent = Number(removeChild.nextElementSibling.textContent) - 1
+    childs--;
+    drdText.textContent = guestsCount(guests) + childsCount(childs)
+
+    if (drdText.textContent === "") {
+      drdText.textContent = 'Сколько гостей'
+    } else if (guestsCount(guests) === "") {
+      drdText.textContent = drdText.textContent.slice(2)
+    }
   }
-})*/
+})
+
+addChild.addEventListener('click', () => {
+  addChild.previousElementSibling.textContent = Number(addChild.previousElementSibling.textContent) + 1
+  childs++;
+  drdText.textContent = guestsCount(guests) + childsCount(childs)
+
+  if (drdText.textContent === "") {
+    drdText.textContent = 'Сколько гостей'
+  } else if (guestsCount(guests) === "") {
+    drdText.textContent = drdText.textContent.slice(2)
+  }
+})
+
+/*прибавление, убавление гостей через querySelectorAll*/ 
+let removeGuest = document.querySelectorAll('.guest-minus')
+let addGuest = document.querySelectorAll('.guest-plus')
+
+removeGuest.forEach((removeButton) => {
+  removeButton.addEventListener('click', () => {
+    if (Number(removeButton.nextElementSibling.textContent) > 0) {
+      removeButton.nextElementSibling.textContent = Number(removeButton.nextElementSibling.textContent) - 1
+      guests--;
+      drdText.textContent = guestsCount(guests) + childsCount(childs)
+
+      if (drdText.textContent === "") {
+        drdText.textContent = 'Сколько гостей'
+      } else if (guestsCount(guests) === "") {
+        drdText.textContent = drdText.textContent.slice(2)
+      }
+    }
+  })
+})
+
+addGuest.forEach((addButton) => {
+  addButton.addEventListener('click', () => {
+    addButton.previousElementSibling.textContent = Number(addButton.previousElementSibling.textContent) + 1
+    guests++;
+
+    drdText.textContent = guestsCount(guests) + childsCount(childs)
+    
+    if (drdText.textContent === "") {
+      drdText.textContent = 'Сколько гостей'
+    } else if (guestsCount(guests) === "") {
+      drdText.textContent = drdText.textContent.slice(2)
+    }
+  })
+})
+
+
+
+/*влияние убавления/прибавления гостей через кнопок на текстовое поле dropdown-field*/ 
+
+/*
+dropDownField.addEventListener('click', () => {
+  guests++;
+  if (guests < 21) {
+    if (guests === 1 || guests === 21) {
+      drdText.textContent = `${guests} гость`
+    } else if (guests > 1 && guests < 5 || guests > 21 && guests < 25) {
+      drdText.textContent = `${guests} гостя`
+    } else {
+      drdText.textContent = `${guests} гостей`
+    }
+  } else {
+    if (/1$/.test(guests)) {
+      drdText.textContent = `${guests} гость`
+    } else if (/[234]$/.test(guests)) {
+      drdText.textContent = `${guests} гостя`
+    } else {
+      drdText.textContent = `${guests} гостей`
+    }
+  }
+})
+*/
+
+
+
+
 
     
 
