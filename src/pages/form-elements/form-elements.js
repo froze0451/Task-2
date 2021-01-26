4419bf6386fc27a5ea5f0b6d12143f99466f9dd4
froze0451 <frozealex@gmail.com>
2021-01-26 23:51:48 +0300

    added calendar model with styles. added simple js 1 month calendar

--- src/pages/form-elements/form-elements.js
+++ src/pages/form-elements/form-elements.js
@@ -175,6 +175,58 @@ addGuest.forEach((addButton) => {
 
 
 
+/*тесты объекта Date */ 
+
+/*window.addEventListener('click', () => {
+  
+  let d = new Date(2021, 0);
+  console.log(d.getDate())
+  console.log(getDay(d))
+
+
+  d.setDate(d.getDate() + 1)
+  console.log(d.getDate())
+  console.log(getDay(d))
+  
+
+  
+  for (let i = 0; i < getDay(d); i++) {
+    if (getDay(d) % 7 == 6)
+    console.log('yeah')
+  }
+
+ function getDay(date) { // получить номер дня недели, от 0 (пн) до 6 (вс)
+    let day = date.getDay();
+    if (day == 0) day = 7; // сделать воскресенье (0) последним днем
+    return day - 1;
+  }
+
+  
+  
+})*/
+
+
+
+
+/*
+window.addEventListener('click', () => {
+  
+  let d = new Date(2021, 0);
+
+  function getDay(date) { // получить номер дня недели, от 0 (пн) до 6 (вс)
+    let day = date.getDay();
+    if (day == 0) day = 7; // сделать воскресенье (0) последним днем
+    return day - 1;
+  }
+
+  for (let i = 0; i < getDay(d); i++) {
+    console.log(d)
+  }
+  
+})
+*/
+
+
 /*влияние убавления/прибавления гостей через кнопок на текстовое поле dropdown-field*/ 
 
 /*
@@ -199,6 +251,61 @@ dropDownField.addEventListener('click', () => {
   }
 })
 */
+
+
+
+
+
+/*вариант календаря для 1 месяца*/ 
+/*
+let calendar = document.getElementById('calendar')
+
+function createCalendar(elem, year, month) {
+
+  let mon = month - 1; // месяцы в JS идут от 0 до 11, а не от 1 до 12
+  let d = new Date(year, mon);
+
+  let table = '<table><tr><th>пн</th><th>вт</th><th>ср</th><th>чт</th><th>пт</th><th>сб</th><th>вс</th></tr><tr>';
+
+  // пробелы для первого ряда
+  // с понедельника до первого дня месяца
+  // * * * 1  2  3  4
+  for (let i = 0; i < getDay(d); i++) {
+    table += '<td></td>';
+  }
+
+  // <td> ячейки календаря с датами
+  while (d.getMonth() == mon) {
+    table += '<td>' + d.getDate() + '</td>';
+
+    if (getDay(d) % 7 == 6) { // вс, последний день - перевод строки
+      table += '</tr><tr>';
+    }
+
+    d.setDate(d.getDate() + 1);
+  }
+
+  // добить таблицу пустыми ячейками, если нужно
+  // 29 30 31 * * * *
+  if (getDay(d) != 0) {
+    for (let i = getDay(d); i < 7; i++) {
+      table += '<td></td>';
+    }
+  }
+
+  // закрыть таблицу
+  table += '</tr></table>';
+
+  elem.innerHTML = table;
+}
+
+function getDay(date) { // получить номер дня недели, от 0 (пн) до 6 (вс)
+  let day = date.getDay();
+  if (day == 0) day = 7; // сделать воскресенье (0) последним днем
+  return day - 1;
+}
+
+createCalendar(calendar, 2021, 1); */
 
 
 
