162ffd3ad2c1b5eb315fe7bf73785fe5ee0d276a
froze0451 <frozealex@gmail.com>
2021-02-17 21:55:49 +0300

    added filter-date dropdown, subscription field, room-facilities dropdown. text-field and dropdown classes and styles was changed universally

--- src/pages/form-elements/form-elements.js
+++ src/pages/form-elements/form-elements.js
@@ -1,28 +1,30 @@
 import '../../styles/style.scss'
 import './form-elements.scss'
 import '../assets/logo.scss'
-import IMask from 'imask';
+import IMask from 'imask'
 
-/* переменные */ 
-let dropDownField = document.querySelector('.dropdown-field') 
+/* переменные */
+let dropDownField = document.querySelector('.dropdown-field')
 let drdText = document.querySelector('.dropdown-field > p')
 let dropDowmMenuButton = document.querySelector('.drop-button')
 let dropDownMenu = document.querySelector('.dropdown-menu')
 
-let doubleDataDropdownButtons = document.querySelectorAll('.date-drowdown .drop-button');
-let wholeCalendar = document.querySelector('.calendar');
+let doubleDataDropdownButtons = document.querySelectorAll('.date-drowdown .drop-button')
+let wholeCalendar = document.querySelector('.calendar')
 
-let guests = 0;
-let childs = 0;
+let guests = 0
+let childs = 0
 
-/*функции*/ 
-function guestsCount(guests) {
+/*функции*/
+
+/* для гест дропдаун*/
+function guestsCount(guests) { 
   if (guests < 21) {
     if (guests === 0) {
       return ''
     } else if (guests === 1 || guests === 21) {
       return `${guests} гость`
-    } else if (guests > 1 && guests < 5 || guests > 21 && guests < 25) {
+    } else if ((guests > 1 && guests < 5) || (guests > 21 && guests < 25)) {
       return `${guests} гостя`
     } else {
       return `${guests} гостей`
@@ -31,7 +33,7 @@ function guestsCount(guests) {
     if (/1$/.test(guests)) {
       return `${guests} гость`
     } else if (/[234]$/.test(guests)) {
-      return`${guests} гостя`
+      return `${guests} гостя`
     } else {
       return `${guests} гостей`
     }
@@ -41,10 +43,10 @@ function guestsCount(guests) {
 function childsCount(childs) {
   if (childs < 21) {
     if (childs === 0) {
-      return ""
+      return ''
     } else if (childs === 1 || childs === 21) {
       return `, ${childs} младенец`
-    } else if (childs > 1 && childs < 5 || childs > 21 && childs < 25) {
+    } else if ((childs > 1 && childs < 5) || (childs > 21 && childs < 25)) {
       return `, ${childs} младенца`
     } else {
       return `, ${childs} младенцев`
@@ -60,56 +62,40 @@ function childsCount(childs) {
   }
 }
 
+/* E V E N T S */
 
-/*эвенты*/ 
+/*маска для поля даты*/
+
+let dateMask = IMask(document.querySelector('.text-field-masked input'), {
+  mask: Date,
+  min: new Date(1990, 0, 1),
+  max: new Date(2021, 0, 1),
+  lazy: true,
+})
+
+/* GUESTS DROPDOWN */
+
+/* скрипты выпадения меню */
 
 dropDowmMenuButton.addEventListener('click', () => {
   dropDownMenu.classList.toggle('invisible')
 
   if (dropDownMenu.classList.contains('invisible')) {
-    dropDownField.style.borderRadius = '4px 4px 4px 4px';
+    dropDownField.style.borderRadius = '4px 4px 4px 4px'
   } else {
-    dropDownField.style.borderRadius = '4px 4px 0 0';
+    dropDownField.style.borderRadius = '4px 4px 0 0'
   }
 })
 
 dropDownField.addEventListener('mouseout', () => {
   if (dropDownMenu.classList.contains('invisible')) {
-    dropDownField.style.border = '1px solid rgba(31, 32, 65, 0.25)';
-  } 
-})
-
-dropDownField.addEventListener('mouseover', () => {
-  dropDownField.style.border = '1px solid rgba(31, 32, 65, 0.5)';
-})
-
-
-/*маска для поля даты*/
-
-let dateMask = IMask(
-  document.querySelector('.text-field-masked input'),
-  {
-    mask: Date,
-    min: new Date(1990, 0, 1),
-    max: new Date(2021, 0, 1),
-    lazy: true
-  });
-
-/*дропдаун меню кнопки добавить/убрать*/ 
-
-/*let removeButton = document.querySelector('.minus')
-let addButton = document.querySelector('.plus')
-
-removeButton.addEventListener('click', () => {
-  if (Number(addButton.previousElementSibling.textContent) > 0) {
-    addButton.previousElementSibling.textContent = Number(addButton.previousElementSibling.textContent) - 1
+    dropDownField.style.border = '1px solid rgba(31, 32, 65, 0.25)'
   }
 })
 
-addButton.addEventListener('click', () => {
-  addButton.previousElementSibling.textContent = Number(addButton.previousElementSibling.textContent) + 1
-})*/
-
+dropDownField.addEventListener('mouseover', () => {
+  dropDownField.style.border = '1px solid rgba(31, 32, 65, 0.5)'
+})
 
 /* подсчет младенцев */
 let removeChild = document.querySelector('.child-minus')
@@ -117,44 +103,47 @@ let addChild = document.querySelector('.child-plus')
 
 removeChild.addEventListener('click', () => {
   if (Number(removeChild.nextElementSibling.textContent) > 0) {
-    removeChild.nextElementSibling.textContent = Number(removeChild.nextElementSibling.textContent) - 1
-    childs--;
+    removeChild.nextElementSibling.textContent =
+      Number(removeChild.nextElementSibling.textContent) - 1
+    childs--
     drdText.textContent = guestsCount(guests) + childsCount(childs)
 
-    if (drdText.textContent === "") {
+    if (drdText.textContent === '') {
       drdText.textContent = 'Сколько гостей'
-    } else if (guestsCount(guests) === "") {
+    } else if (guestsCount(guests) === '') {
       drdText.textContent = drdText.textContent.slice(2)
     }
   }
 })
 
 addChild.addEventListener('click', () => {
-  addChild.previousElementSibling.textContent = Number(addChild.previousElementSibling.textContent) + 1
-  childs++;
+  addChild.previousElementSibling.textContent =
+    Number(addChild.previousElementSibling.textContent) + 1
+  childs++
   drdText.textContent = guestsCount(guests) + childsCount(childs)
 
-  if (drdText.textContent === "") {
+  if (drdText.textContent === '') {
     drdText.textContent = 'Сколько гостей'
-  } else if (guestsCount(guests) === "") {
+  } else if (guestsCount(guests) === '') {
     drdText.textContent = drdText.textContent.slice(2)
   }
 })
 
-/*прибавление, убавление гостей через querySelectorAll*/ 
+/*прибавление, убавление гостей через querySelectorAll*/
 let removeGuest = document.querySelectorAll('.guest-minus')
 let addGuest = document.querySelectorAll('.guest-plus')
 
 removeGuest.forEach((removeButton) => {
   removeButton.addEventListener('click', () => {
     if (Number(removeButton.nextElementSibling.textContent) > 0) {
-      removeButton.nextElementSibling.textContent = Number(removeButton.nextElementSibling.textContent) - 1
-      guests--;
+      removeButton.nextElementSibling.textContent =
+        Number(removeButton.nextElementSibling.textContent) - 1
+      guests--
       drdText.textContent = guestsCount(guests) + childsCount(childs)
 
-      if (drdText.textContent === "") {
+      if (drdText.textContent === '') {
         drdText.textContent = 'Сколько гостей'
-      } else if (guestsCount(guests) === "") {
+      } else if (guestsCount(guests) === '') {
         drdText.textContent = drdText.textContent.slice(2)
       }
     }
@@ -163,179 +152,98 @@ removeGuest.forEach((removeButton) => {
 
 addGuest.forEach((addButton) => {
   addButton.addEventListener('click', () => {
-    addButton.previousElementSibling.textContent = Number(addButton.previousElementSibling.textContent) + 1
-    guests++;
+    addButton.previousElementSibling.textContent =
+      Number(addButton.previousElementSibling.textContent) + 1
+    guests++
 
     drdText.textContent = guestsCount(guests) + childsCount(childs)
-    
-    if (drdText.textContent === "") {
+
+    if (drdText.textContent === '') {
       drdText.textContent = 'Сколько гостей'
-    } else if (guestsCount(guests) === "") {
+    } else if (guestsCount(guests) === '') {
       drdText.textContent = drdText.textContent.slice(2)
     }
   })
 })
 
-
-
-/*тесты объекта Date */ 
-
-/*window.addEventListener('click', () => {
-  
-  let d = new Date(2021, 0);
-  console.log(d.getDate())
-  console.log(getDay(d))
-
-
-  d.setDate(d.getDate() + 1)
-  console.log(d.getDate())
-  console.log(getDay(d))
-  
-
-  
-  for (let i = 0; i < getDay(d); i++) {
-    if (getDay(d) % 7 == 6)
-    console.log('yeah')
-  }
-
- function getDay(date) { // получить номер дня недели, от 0 (пн) до 6 (вс)
-    let day = date.getDay();
-    if (day == 0) day = 7; // сделать воскресенье (0) последним днем
-    return day - 1;
-  }
-
-  
-  
-})*/
-
-
-
-
-/*
-window.addEventListener('click', () => {
-  
-  let d = new Date(2021, 0);
-
-  function getDay(date) { // получить номер дня недели, от 0 (пн) до 6 (вс)
-    let day = date.getDay();
-    if (day == 0) day = 7; // сделать воскресенье (0) последним днем
-    return day - 1;
-  }
-
-  for (let i = 0; i < getDay(d); i++) {
-    console.log(d)
-  }
-  
-})
-*/
-
-
-/*влияние убавления/прибавления гостей через кнопок на текстовое поле dropdown-field*/ 
-
-/*
-dropDownField.addEventListener('click', () => {
-  guests++;
-  if (guests < 21) {
-    if (guests === 1 || guests === 21) {
-      drdText.textContent = `${guests} гость`
-    } else if (guests > 1 && guests < 5 || guests > 21 && guests < 25) {
-      drdText.textContent = `${guests} гостя`
-    } else {
-      drdText.textContent = `${guests} гостей`
-    }
-  } else {
-    if (/1$/.test(guests)) {
-      drdText.textContent = `${guests} гость`
-    } else if (/[234]$/.test(guests)) {
-      drdText.textContent = `${guests} гостя`
-    } else {
-      drdText.textContent = `${guests} гостей`
-    }
-  }
-})
-*/
-
-
-
+/* calendar */
 
 let previousMonthButton = document.querySelector('.previous-month-button')
 let nextMonthButton = document.querySelector('.next-month-button')
 
-
-
-
-
-/*вариант календаря для 1 месяца*/ 
+/*вариант календаря для 1 месяца*/
 
 let calendar = document.getElementById('calendar')
 
-function createCalendar() { /*(elem, year, month)*/
+function createCalendar() {
+  /*(elem, year, month)*/
 
-  let coco = 0;
+  let coco = 0 /*let d = new Date(year, month); */ // месяцы в JS идут от 0 до 11, а не от 1 до 12
 
-  /*let mon = month - 1; */// месяцы в JS идут от 0 до 11, а не от 1 до 12
-  /*let d = new Date(year, month); */
-  let d = new Date(); 
-  let today = d.getDay();
-  
+  /*let mon = month - 1; */ let d = new Date()
+  let today = d.getDate()
+  d.setDate(1)
   /*if (next) {
     d.setMonth(d.getMonth() + 1);
   }*/
-  
-  let mon = d.getMonth(); 
 
-  let pp = new Date();
-  pp.setDate(d.getDate() - getDay(d));
-  
-  
-  let table = '<tr>';
+  let mon = d.getMonth()
 
-  
+  let pp = new Date()
+  /*pp.setDate(d.getDate() - getDay(d));*/
+  pp.setDate(d.getDate() - getDay(d))
+  console.log(pp)
+
+  let table = '<tr>'
+
   /*for (let i = 0; i < getDay(d); i++) {
     table += `<td>` + pp.getDate() + `</td>`;
     pp.setDate(pp.getDate() - 1);
   }*/
-  
-  
+
   // пробелы для первого ряда
   // с понедельника до первого дня месяца
   // * * * 1  2  3  4
   for (let i = 0; i < getDay(d); i++) {
-   table += `<td>` + pp.getDate() + `</td>`;
-    pp.setDate(pp.getDate() + 1);
-    coco++;
-    console.log(coco);
+    table += `<td>` + pp.getDate() + `</td>`
+    pp.setDate(pp.getDate() + 1)
+    coco++
+    console.log(coco)
+    console.log(getDay(d))
   }
 
   // <td> ячейки календаря с датами
   while (d.getMonth() == mon) {
     if (d.getDate() === today) {
-      table += '<td style="border-radius: 50%; background: linear-gradient(180deg, #6FCF97 0%, #66D2EA 100%); color: #FFFFFF">' + d.getDate() + '</td>';
+      table +=
+        '<td style="border-radius: 50%; background: linear-gradient(180deg, #6FCF97 0%, #66D2EA 100%); color: #FFFFFF">' +
+        d.getDate() +
+        '</td>'
     } else {
-      table += '<td>' + d.getDate() + '</td>';
+      table += '<td>' + d.getDate() + '</td>'
     }
-    
 
-    if (getDay(d) % 7 == 6) { // вс, последний день - перевод строки
-      table += '</tr><tr>';
+    if (getDay(d) % 7 == 6) {
+      // вс, последний день - перевод строки
+      table += '</tr><tr>'
     }
-    
-    d.setDate(d.getDate() + 1);
-    coco++;
+
+    d.setDate(d.getDate() + 1)
+    coco++
   }
 
   if (coco === 28) {
     for (let i = getDay(d); i < 7; i++) {
-      table += '<td>' + d.getDate() + '</td>';
-      d.setDate(d.getDate() + 1);
-      coco++;
+      table += '<td>' + d.getDate() + '</td>'
+      d.setDate(d.getDate() + 1)
+      coco++
     }
   } else {
     if (getDay(d) != 0) {
       for (let i = getDay(d); i < 7; i++) {
-        table += '<td>' + d.getDate() + '</td>';
-        d.setDate(d.getDate() + 1);
-        coco++;
+        table += '<td>' + d.getDate() + '</td>'
+        d.setDate(d.getDate() + 1)
+        coco++
       }
     }
   }
@@ -350,8 +258,8 @@ function createCalendar() { /*(elem, year, month)*/
     }
   }*/
 
-  console.log(coco);
-  
+  console.log(coco)
+
   //добиваем начало следующего месяца
   /*if (coco === 28) {
     table += '<tr>';
@@ -363,24 +271,24 @@ function createCalendar() { /*(elem, year, month)*/
     coco++;
   }*/
 
-  console.log(coco);
+  console.log(coco)
 
   // закрыть таблицу
-  table += '</tr>';
+  table += '</tr>'
 
   /*elem.innerHTML = table;*/
-  calendar.innerHTML = table;
-  
+  calendar.innerHTML = table
 }
 
-function getDay(date) { // получить номер дня недели, от 0 (пн) до 6 (вс)
-  let day = date.getDay();
-  if (day == 0) day = 7; // сделать воскресенье (0) последним днем
-  return day - 1;
+function getDay(date) {
+  // получить номер дня недели, от 0 (пн) до 6 (вс)
+  let day = date.getDay()
+  if (day == 0) day = 7 // сделать воскресенье (0) последним днем
+  return day - 1
 }
 
 /*createCalendar(calendar, 2021, 1); */
-createCalendar(); 
+createCalendar()
 
 /*
 previousMonthButton.addEventListener('click', () => {
@@ -391,50 +299,48 @@ nextMonthButton.addEventListener('click', () => {
   createCalendar(); 
 })*/
 
+let checkIn
+let checkOut
+let checkInDate
+let cells = document.querySelectorAll('.calendar tbody td')
 
-let checkIn;
-let checkOut;
-let checkInDate;
-let cells = document.querySelectorAll('.calendar tbody td');
-
-cells.forEach((cell) => {     
+cells.forEach((cell) => {
   cell.addEventListener('click', () => {
-    cell.style.background = 'linear-gradient(180deg, #BC9CFF 0%, #8BA4F9 100%)';
-    cell.style.borderRadius = '50%';
-    
-    
-    checkIn = [].indexOf.call(cells, cell);
-    checkInDate = checkIn + 1;
-    
-    console.log(checkIn);
-    console.log(checkInDate);
-    
+    cell.style.background = 'linear-gradient(180deg, #BC9CFF 0%, #8BA4F9 100%)'
+    cell.style.borderRadius = '50%'
+
+    checkIn = [].indexOf.call(cells, cell)
+    checkInDate = checkIn + 1
+
+    console.log(checkIn)
+    console.log(checkInDate)
   })
 })
 
 cells.forEach((cell) => {
   cell.addEventListener('mouseover', () => {
     if (checkIn) {
-      console.log('nice');
-      let index = [].indexOf.call(cells, cell);
-      if (checkIn < index) {     
+      console.log('nice')
+      let index = [].indexOf.call(cells, cell)
+      if (checkIn < index) {
         for (; index > checkIn; index--) {
-          cells[index].style.background = 'linear-gradient(180deg, #BC9CFF 0%, #8BA4F9 100%)';
-          cells[index].style.opacity = '0.25';
+          cells[index].style.background =
+            'linear-gradient(180deg, #BC9CFF 0%, #8BA4F9 100%)'
+          cells[index].style.opacity = '0.25'
         }
-      } 
+      }
     }
   })
-  
+
   cell.addEventListener('mouseout', () => {
     if (checkIn) {
-      console.log('nice');
-      let index = [].indexOf.call(cells, cell);
-      if (checkIn < index) {     
+      console.log('nice')
+      let index = [].indexOf.call(cells, cell)
+      if (checkIn < index) {
         for (; index > checkIn; index--) {
-          cells[index].style.background = 'white';
+          cells[index].style.background = 'white'
         }
-      } 
+      }
     }
   })
 })
@@ -442,33 +348,22 @@ cells.forEach((cell) => {
 cells.forEach((cell) => {
   cell.addEventListener('click', () => {
     if (checkIn) {
-      let afterDate = [].indexOf.call(cells, cell);
+      let afterDate = [].indexOf.call(cells, cell)
       if (afterDate < checkIn) {
         checkIn = afterDate
       } else {
-        checkOut = afterDate;
-        cell.style.background = 'linear-gradient(180deg, #BC9CFF 0%, #8BA4F9 100%)';
-        cell.style.borderRadius = '50%';
-        cell.style.opacity= '1';
+        checkOut = afterDate
+        cell.style.background =
+          'linear-gradient(180deg, #BC9CFF 0%, #8BA4F9 100%)'
+        cell.style.borderRadius = '50%'
+        cell.style.opacity = '1'
       }
     }
   })
 })
-
 
 doubleDataDropdownButtons.forEach((button) => {
   button.addEventListener('click', () => {
     wholeCalendar.classList.toggle('invisible')
   })
 })
-
-
-
-
-
-
-
-
-
-    
-
